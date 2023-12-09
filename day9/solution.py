2a64d23b620373ae7fe05cb16f1cddd560839351
Michal Przysucha <mprzysucha@plume.com>
2023-12-09 11:02:23 +0100

    day9

--- day9/solution.py
+++ day9/solution.py
@@ -1,19 +1,50 @@
 import requests
 
-def solve_part_1():
-    counter = 0
-    return counter
+def calculate_lines_below(nums_below):
+    lines_below = []
+    while len(lines_below) == 0 or not all(map(lambda x: x == 0, lines_below[len(lines_below) - 1])):
+        prev_nums_below = nums_below
+        nums_below = []
+        prev_num = None
+        for num in prev_nums_below:
+            if prev_num is not None:
+                nums_below.append(num - prev_num)
+            prev_num = num
+        lines_below.append(nums_below)
+    return lines_below
+
+def solve_part_1(input_lines):
+    sum = 0
+    for line in input_lines:
+        nums_below = list(map(int, line.split()))
+        last_num = nums_below[-1]
+        lines_below = calculate_lines_below(nums_below)
+        prev_num = 0
+        for nums_below in lines_below[::-1]:
+            prev_num += nums_below[-1]
+        prev_num += last_num
+        sum += prev_num
+    return sum
 
 
-def solve_part_2():
-    counter = 0
-    return counter
+def solve_part_2(input_lines):
+    sum = 0
+    for line in input_lines:
+        nums_below = list(map(int, line.split()))
+        last_num = nums_below[-1]
+        first_num = nums_below[0]
+        lines_below = calculate_lines_below(nums_below)
+        prev_num = 0
+        for nums_below in lines_below[::-1]:
+            prev_num = nums_below[0] - prev_num
+        prev_num = first_num - prev_num
+        sum += prev_num
+    return sum
 
 def main():
-    # input = read_input()
-    input = open("test_input").readlines()
-
-    return solve_part_1(), solve_part_2()
+    input = read_input()
+    # input = open("test_input").readlines()
+    return solve_part_1(input), solve_part_2(input)
 
 def read_input():
     headers = {'Cookie': 'session={}'.format(open("../sessionId").readline())}
